d9657794f232d485566b38d8aa1adbe08f5825ba
shirinkalam <mahdishiri869@gmail.com>
2022-08-16 11:50:55 +0430

    add login and regester Ability and permissoin Assign to user implemented

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
+        auth()->user()->givePermissionsTo(['delete posts','delete users']);
     return view('welcome');
+})->name('home');
+
+Route::prefix('auth')->namespace('Auth')->group(function () {
+    Route::get('register',[RegisterController::class,'showRegisterForm'])->name('auth.register.form');
+    Route::post('register',[RegisterController::class,'register'])->name('auth.register');
+    Route::get('login',[LoginController::class,'showLoginForm'])->name('auth.login.form');
+    Route::post('login',[LoginController::class,'login'])->name('auth.login');
+    Route::get('logout',[LoginController::class,'logout'])->name('auth.logout');
 });
